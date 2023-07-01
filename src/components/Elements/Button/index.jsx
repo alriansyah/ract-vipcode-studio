eb896fffdeadca5b4a29cf563f5d36e8a97fbb1f
alriansyah <alriansyah1218@gmail.com>
2023-07-01 23:17:41 +0700

    Atomic Design

---
+++ src/components/Elements/Button/index.jsx
@@ -0,0 +1,8 @@
+export default function Button (props) {
+    const { children = "....", variant = "bg-black" } = props;
+    return (
+        <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
+            {children}
+        </button>
+    )
+}
