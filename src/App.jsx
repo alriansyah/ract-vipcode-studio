e563903ac87c6ffedbec41d7e2de9947a776de00
alriansyah <alriansyah1218@gmail.com>
2023-07-01 22:40:55 +0700

    Components and Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = (props) => {
+  const { children = "....", variant = "bg-black" } = props;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
+      {children}
+    </button>
   )
 }
 
-export default App
+
+
+export default function App() {
+  return (
+    <div className='flex justify-center gap-4 bg-blue-700 min-h-screen items-center'>
+      <Button variant="bg-red-700">Login</Button>
+      <Button variant="bg-slate-700">Register</Button>
+      <Button variant="bg-black">Logout</Button>
+    </div>
+  )
+}
