78761c667c666fdb9f3060d3083a91b68dce6294
alriansyah <alriansyah1218@gmail.com>
2023-07-09 23:07:39 +0700

    Rendering List

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,39 +1,54 @@
 import React from 'react';
 import CardProduct from '../components/Fragments/CardProduct';
+
+
+const products = [
+    {
+        id: 1,
+        title: "Sepatu Baru",
+        price: "Rp. 1.000.000",
+        image: "/images/shoes.jpg",
+        description: `lorem lorem lorem lorem
+        lorem lorem lorem lorem
+        lorem lorem lorem lorem
+        lorem lorem lorem lorem
+        lorem lorem lorem lorem
+        lorem lorem lorem lorem
+        lorem lorem lorem lorem
+        lorem lorem lorem lorem
+        lorem lorem lorem lorem`
+    },
+    {
+        id: 2,
+        title: "Sepatu Lama",
+        price: "Rp. 500.000",
+        image: "/images/shoes.jpg",
+        description: `lorem lorem lorem lorem
+        lorem lorem lorem lorem`
+    },
+    {
+        id: 3,
+        title: "Sepatu Lama",
+        price: "Rp. 500.000",
+        image: "/images/shoes.jpg",
+        description: `lorem lorem lorem lorem
+        lorem lorem lorem lorem`
+    },
+];
+
+
 export default function ProductPage() {
     return (
         <div className='flex justify-center py-5'>
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes.jpg" />
-                <CardProduct.Body title="Sepatu Baru">
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp 1.000.000" />
-            </CardProduct>
-
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes.jpg" />
-                <CardProduct.Body title="Sepatu Baru">
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                    lorem lorem lorem lorem
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp 1.000.000" />
-            </CardProduct>
+            {products.map((product) => (
+                <CardProduct>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body name={product.name}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price} />
+                </CardProduct>
+            ))}
         </div>
     )
 }
